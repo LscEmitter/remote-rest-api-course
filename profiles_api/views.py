06772107455e903f43b34e14690b8c3653ddf30e
Lothar Schlüter <git@entkam.de>
2022-04-28 11:17:55 +0200

    Added added /api/login - ModHeader module needed to test

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -50,6 +52,7 @@ class HelloApiView(APIView):
         """Delete an object"""
         return Response({'method': 'DELETE'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
 
@@ -64,7 +67,7 @@ class HelloViewSet(viewsets.ViewSet):
             'Provides more functionality with less code',
         ]
 
-        return Response({'message' : 'Hello!', 'a_viewset' : a_viewset})
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
     def create(self, request):
         """Create a new hello message"""
@@ -72,7 +75,7 @@ class HelloViewSet(viewsets.ViewSet):
 
         if serializer.is_valid():
             message = f'Hello {serializer.validated_data.get("name")}'
-            return Response({'message' : message})
+            return Response({'message': message})
         else:
             return Response(
                 serializer.errors,
@@ -81,19 +84,20 @@ class HelloViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
-        return Response({'http_method' : 'GET'})
+        return Response({'http_method': 'GET'})
 
     def update(self, request, pk=None):
         """Handle updating an object"""
-        return Response({'http_method' : 'PUT'})
+        return Response({'http_method': 'PUT'})
 
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
-        return Response({'http_method' : 'PATCH'})
+        return Response({'http_method': 'PATCH'})
 
     def destroy(self, request, pk=None):
         """Handle removing an object"""
-        return Response({'http_method' : 'DELETE'})
+        return Response({'http_method': 'DELETE'})
+
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
@@ -103,3 +107,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
