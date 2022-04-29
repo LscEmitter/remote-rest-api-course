a4031e7829f2e4ece49e25510bf66b3d83afc238
Lothar Schlüter <git@entkam.de>
2022-04-29 13:01:40 +0200

    Added read and write permissions for profile feeds

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow user do edit his own profile"""
 
@@ -9,3 +10,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         print(f"=== request.user.id = {request.user.id}")
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allows users ti update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
+
